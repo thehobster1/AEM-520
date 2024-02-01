ed245370204816c9c620d7b33a2a213d8ab558c5
thehobster1 <115751963+thehobster1@users.noreply.github.com>
2024-02-01 14:47:21 -0600

    Add files via upload

--- AEM520HW2Plot.m
+++ AEM520HW2Plot.m
@@ -10,43 +10,43 @@ plotCSV('cos_100.csv', 'cos_100');
 plotCSV('cos_1000.csv', 'cos_1000');
 
 
-plotCSV('der1_2_10.csv', 'der1_2_10');
+plotCSV('der_1_2_10.csv', 'der_1_2_10');
 
-plotCSV('der1_2_10_error.csv', 'der1_2_10_error');
+plotCSV('der_1_2_10_error.csv', 'der_1_2_10_error');
 
-plotCSV('der1_2_100.csv', 'der1_2_100');
+plotCSV('der_1_2_100.csv', 'der_1_2_100');
 
-plotCSV('der1_2_100_error.csv', 'der1_2_100_error');
+plotCSV('der_1_2_100_error.csv', 'der_1_2_100_error');
 
-plotCSV('der1_2_1000.csv', 'der1_2_1000');
+plotCSV('der_1_2_1000.csv', 'der_1_2_1000');
 
-plotCSV('der1_2_1000_error.csv', 'der1_2_1000_error');
-
-
-plotCSV('der1_4_10.csv', 'der1_4_10');
-
-plotCSV('der1_4_10_error.csv', 'der1_4_10_error');
-
-plotCSV('der1_4_100.csv', 'der1_4_100');
-
-plotCSV('der1_4_100_error.csv', 'der1_4_100_error');
-
-plotCSV('der1_4_1000.csv', 'der1_4_1000');
-
-plotCSV('der1_4_1000_error.csv', 'der1_4_1000_error');
+plotCSV('der_1_2_1000_error.csv', 'der_1_2_1000_error');
 
 
-plotCSV('der1_6_10.csv', 'der1_6_10');
+plotCSV('der_1_4_10.csv', 'der_1_4_10');
 
-plotCSV('der1_6_10_error.csv', 'der1_6_10_error');
+plotCSV('der_1_4_10_error.csv', 'der_1_4_10_error');
 
-plotCSV('der1_6_100.csv', 'der1_6_100');
+plotCSV('der_1_4_100.csv', 'der_1_4_100');
 
-plotCSV('der1_6_100_error.csv', 'der1_6_100_error');
+plotCSV('der_1_4_100_error.csv', 'der_1_4_100_error');
 
-plotCSV('der1_6_1000.csv', 'der1_6_1000');
+plotCSV('der_1_4_1000.csv', 'der_1_4_1000');
 
-plotCSV('der1_6_1000_error.csv', 'der1_6_1000_error');
+plotCSV('der_1_4_1000_error.csv', 'der_1_4_1000_error');
+
+
+plotCSV('der_1_6_10.csv', 'der_1_6_10');
+
+plotCSV('der_1_6_10_error.csv', 'der_1_6_10_error');
+
+plotCSV('der_1_6_100.csv', 'der_1_6_100');
+
+plotCSV('der_1_6_100_error.csv', 'der_1_6_100_error');
+
+plotCSV('der_1_6_1000.csv', 'der_1_6_1000');
+
+plotCSV('der_1_6_1000_error.csv', 'der_1_6_1000_error');
 
 
 
@@ -57,41 +57,41 @@ plotCSV('sin_100.csv', 'sin_100');
 plotCSV('sin_1000.csv', 'sin_1000');
 
 
-plotCSV('der2_2_10.csv', 'der2_2_10');
+plotCSV('der_2_2_10.csv', 'der_2_2_10');
 
-plotCSV('der2_2_10_error.csv', 'der2_2_10_error');
+plotCSV('der_2_2_10_error.csv', 'der_2_2_10_error');
 
-plotCSV('der2_2_100.csv', 'der2_2_100');
+plotCSV('der_2_2_100.csv', 'der_2_2_100');
 
-plotCSV('der2_2_100_error.csv', 'der2_2_100_error');
+plotCSV('der_2_2_100_error.csv', 'der_2_2_100_error');
 
-plotCSV('der2_2_1000.csv', 'der2_2_1000');
+plotCSV('der_2_2_1000.csv', 'der_2_2_1000');
 
-plotCSV('der2_2_1000_error.csv', 'der2_2_1000_error');
-
-
-plotCSV('der2_4_10.csv', 'der2_4_10');
-
-plotCSV('der2_4_10_error.csv', 'der2_4_10_error');
-
-plotCSV('der2_4_100.csv', 'der2_4_100');
-
-plotCSV('der2_4_100_error.csv', 'der2_4_100_error');
-
-plotCSV('der2_4_1000.csv', 'der2_4_1000');
-
-plotCSV('der2_4_1000_error.csv', 'der2_4_1000_error');
+plotCSV('der_2_2_1000_error.csv', 'der_2_2_1000_error');
 
 
-plotCSV('der2_6_10.csv', 'der2_6_10');
+plotCSV('der_2_4_10.csv', 'der_2_4_10');
 
-plotCSV('der2_6_10_error.csv', 'der2_6_10_error');
+plotCSV('der_2_4_10_error.csv', 'der_2_4_10_error');
 
-plotCSV('der2_6_100.csv', 'der2_6_100');
+plotCSV('der_2_4_100.csv', 'der_2_4_100');
 
-plotCSV('der2_6_100_error.csv', 'der2_6_100_error');
+plotCSV('der_2_4_100_error.csv', 'der_2_4_100_error');
 
-plotCSV('der2_6_1000.csv', 'der2_6_1000');
+plotCSV('der_2_4_1000.csv', 'der_2_4_1000');
 
-plotCSV('der2_6_1000_error.csv', 'der2_6_1000_error');
+plotCSV('der_2_4_1000_error.csv', 'der_2_4_1000_error');
+
+
+plotCSV('der_2_6_10.csv', 'der_2_6_10');
+
+plotCSV('der_2_6_10_error.csv', 'der_2_6_10_error');
+
+plotCSV('der_2_6_100.csv', 'der_2_6_100');
+
+plotCSV('der_2_6_100_error.csv', 'der_2_6_100_error');
+
+plotCSV('der_2_6_1000.csv', 'der_2_6_1000');
+
+plotCSV('der_2_6_1000_error.csv', 'der_2_6_1000_error');
 
