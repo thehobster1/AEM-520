d3f136e9f7ebe2f50622e2ec55f8641eb300a924
thehobster1 <115751963+thehobster1@users.noreply.github.com>
2024-02-01 14:38:21 -0600

    Add files via upload

--- AEM520HW2Plot.m
+++ AEM520HW2Plot.m
@@ -1,7 +1,97 @@
 clear all
 close all
 
-plotCSV('testPlot.csv', '1');
+i = 1;
 
-plotCSV('testPlot2.csv', '2');
+plotCSV('cos_10.csv', 'cos_10');
+
+plotCSV('cos_100.csv', 'cos_100');
+
+plotCSV('cos_1000.csv', 'cos_1000');
+
+
+plotCSV('der1_2_10.csv', 'der1_2_10');
+
+plotCSV('der1_2_10_error.csv', 'der1_2_10_error');
+
+plotCSV('der1_2_100.csv', 'der1_2_100');
+
+plotCSV('der1_2_100_error.csv', 'der1_2_100_error');
+
+plotCSV('der1_2_1000.csv', 'der1_2_1000');
+
+plotCSV('der1_2_1000_error.csv', 'der1_2_1000_error');
+
+
+plotCSV('der1_4_10.csv', 'der1_4_10');
+
+plotCSV('der1_4_10_error.csv', 'der1_4_10_error');
+
+plotCSV('der1_4_100.csv', 'der1_4_100');
+
+plotCSV('der1_4_100_error.csv', 'der1_4_100_error');
+
+plotCSV('der1_4_1000.csv', 'der1_4_1000');
+
+plotCSV('der1_4_1000_error.csv', 'der1_4_1000_error');
+
+
+plotCSV('der1_6_10.csv', 'der1_6_10');
+
+plotCSV('der1_6_10_error.csv', 'der1_6_10_error');
+
+plotCSV('der1_6_100.csv', 'der1_6_100');
+
+plotCSV('der1_6_100_error.csv', 'der1_6_100_error');
+
+plotCSV('der1_6_1000.csv', 'der1_6_1000');
+
+plotCSV('der1_6_1000_error.csv', 'der1_6_1000_error');
+
+
+
+plotCSV('sin_10.csv', 'sin_10');
+
+plotCSV('sin_100.csv', 'sin_100');
+
+plotCSV('sin_1000.csv', 'sin_1000');
+
+
+plotCSV('der2_2_10.csv', 'der2_2_10');
+
+plotCSV('der2_2_10_error.csv', 'der2_2_10_error');
+
+plotCSV('der2_2_100.csv', 'der2_2_100');
+
+plotCSV('der2_2_100_error.csv', 'der2_2_100_error');
+
+plotCSV('der2_2_1000.csv', 'der2_2_1000');
+
+plotCSV('der2_2_1000_error.csv', 'der2_2_1000_error');
+
+
+plotCSV('der2_4_10.csv', 'der2_4_10');
+
+plotCSV('der2_4_10_error.csv', 'der2_4_10_error');
+
+plotCSV('der2_4_100.csv', 'der2_4_100');
+
+plotCSV('der2_4_100_error.csv', 'der2_4_100_error');
+
+plotCSV('der2_4_1000.csv', 'der2_4_1000');
+
+plotCSV('der2_4_1000_error.csv', 'der2_4_1000_error');
+
+
+plotCSV('der2_6_10.csv', 'der2_6_10');
+
+plotCSV('der2_6_10_error.csv', 'der2_6_10_error');
+
+plotCSV('der2_6_100.csv', 'der2_6_100');
+
+plotCSV('der2_6_100_error.csv', 'der2_6_100_error');
+
+plotCSV('der2_6_1000.csv', 'der2_6_1000');
+
+plotCSV('der2_6_1000_error.csv', 'der2_6_1000_error');
 
