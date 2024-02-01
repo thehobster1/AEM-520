c1c5ee84747112cc0f22e2aae8ea8eac19ee0238
thehobster1 <115751963+thehobster1@users.noreply.github.com>
2024-02-01 17:48:57 -0600

    Add files via upload

--- plotCSV3.m
+++ plotCSV3.m
@@ -2,6 +2,8 @@ function plotCSV3(file, name)
 
     figure('Name', name);
 
+    hold on
+
     A = readmatrix(append(file,'_10.csv'));
     B = readmatrix(append(file,'_100.csv'));
     C = readmatrix(append(file,'_1000.csv'));
@@ -10,6 +12,8 @@ function plotCSV3(file, name)
     plot (A(:,1),A(:,2));
     plot (B(:,1),B(:,2));
     plot (C(:,1),C(:,2));
+
+    hold off
 
 
 end
