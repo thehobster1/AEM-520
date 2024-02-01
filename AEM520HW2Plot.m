af1c9441d803ee929c8d1bfbacf4d87913fa2a88
thehobster1 <115751963+thehobster1@users.noreply.github.com>
2024-02-01 17:36:18 -0600

    Update AEM520HW2Plot.m

--- AEM520HW2Plot.m
+++ AEM520HW2Plot.m
@@ -12,19 +12,19 @@ plotCSV('cos_100.csv', 'cos_100');
 plotCSV('cos_1000.csv', 'cos_1000');
 
 
-plotCSV('der_1_2_', 'der_1_2');
+plotCSV3('der_1_2', 'der_1_2');
 
-plotCSV('der_1_2_error', 'der_1_2_error');
-
-
-plotCSV('der_1_4', 'der_1_4');
-
-plotCSV('der_1_4_error', 'der_1_4_error');
+plotCSV3('der_1_2_error', 'der_1_2_error');
 
 
-plotCSV('der_1_6', 'der_1_6');
+plotCSV3('der_1_4', 'der_1_4');
 
-plotCSV('der_1_6_error', 'der_1_6_error');
+plotCSV3('der_1_4_error', 'der_1_4_error');
+
+
+plotCSV3('der_1_6', 'der_1_6');
+
+plotCSV3('der_1_6_error', 'der_1_6_error');
 
 
 
@@ -35,18 +35,18 @@ plotCSV('sin_100.csv', 'sin_100');
 plotCSV('sin_1000.csv', 'sin_1000');
 
 
-plotCSV('der_2_2', 'der_2_2');
+plotCSV3('der_2_2', 'der_2_2');
 
-plotCSV('der_2_2_error', 'der_2_2_error');
-
-
-plotCSV('der_2_4_10', 'der_2_4');
-
-plotCSV('der_2_4_error', 'der_2_4_error');
+plotCSV3('der_2_2_error', 'der_2_2_error');
 
 
-plotCSV('der_2_6', 'der_2_6');
+plotCSV3('der_2_4_10', 'der_2_4');
 
-plotCSV('der_2_6_error', 'der_2_6_error');
+plotCSV3('der_2_4_error', 'der_2_4_error');
+
+
+plotCSV3('der_2_6', 'der_2_6');
+
+plotCSV3('der_2_6_error', 'der_2_6_error');
 
 
